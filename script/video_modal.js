c6526ddc1136213100cf58c47f2589f1f6bb1c27
AlexMFin <alexey.finohenov@gmail.com>
2019-04-28 22:12:24 +0300

    pages 1 and 2

---
+++ script/video_modal.js
@@ -0,0 +1,22 @@
+function videoModal(link) {
+
+    let v = new URL(link).searchParams.get('v');
+
+    $(`<div class="modal fade" id="myModal" role="dialog"> 
+     <div class="modal-dialog video_modal modal-xl modal-dialog-centered"> 
+       <!-- Modal content--> 
+        <div class="modal-content"> 
+            <iframe width="100%" height="450px" src="https://www.youtube.com/embed/${v}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+ 
+       </div> 
+    </div> 
+  </div>`).appendTo('body');
+
+    //Trigger the modal
+    $("#myModal").modal({});
+
+    //Remove the modal once it is closed.
+    $("#myModal").on('hidden.bs.modal', function () {
+        $("#myModal").remove();
+    });
+}
